4b99d39f44be5e7c3d777d3689058cbbb2bd2e0c
wangyi <3218923350@qq.com>
2025-12-16 00:23:24 +0800

    enum

--- remote_run.sh
+++ remote_run.sh
@@ -3,7 +3,7 @@ set -e
 cd /root/ai4s_enum || exit 1
 source /root/ai4s_enum/.env || exit 1
 
-nohup /opt/mamba/bin/python run.py --all > log 2>&1 --close-fds &
+nohup /opt/mamba/bin/python run.py --all > log 2>&1 &
 
 disown $!
 
